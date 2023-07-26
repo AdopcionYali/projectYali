888a3ff5d8508a7791e69f2b49bbbaecff2f6505
JesusMendoza815 <jesuscamposmendoza815@gmail.com>
2023-07-25 22:40:02 -0600

    project base done

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Yali</title>
+        <meta name="description" content="Adopción de mascotas" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+    </>
+  )
+}
