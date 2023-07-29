c4d14910d4e7e35e989f03412650e3561cccfa4a
JesusMendoza815 <jesuscamposmendoza815@gmail.com>
2023-07-29 13:11:04 -0600

    Project base ready

---
+++ pages/api/server.js
@@ -0,0 +1,14 @@
+import express from  'express';
+import cors from 'cors'
+
+const server = express();
+
+// Middlewares
+server.use(express.json());
+server.use(cors());
+
+// Routers
+server.get('/', (req, res) => res.json('Yali´s server OK!'))
+
+
+export { server }
---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Yali</title>
+        <meta name='description' content='Adopción de mascotas' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.svg' />
+      </Head>
+    </>
+  )
+}
