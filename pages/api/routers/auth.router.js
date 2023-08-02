bb31c0cfba06d470eda5ddfa13c994f2b3c22757
Iyari Maldonado <mffchmono@gmail.com>
2023-08-01 21:38:12 -0600

    feat: added login functionality

--- pages/api/routers/auth.router.js
+++ pages/api/routers/auth.router.js
@@ -1,12 +1,21 @@
-import express from "express"
-import login from "../usecases/auth.usecases.js"
+import express from "express";
+import login from "../usecases/auth.usecases.js";
 
-const routeAuth = express.Router()
+const routeAuth = express.Router();
 
 routeAuth.post('/', async (req, res) => {
-    const {phoneNumber, password} = req.body
-    const token = await login(phoneNumber, password)
-    res.json(token)
-})
+  const { phoneNumber, password } = req.body;
 
-export default routeAuth
+  try {
+    const token = await login(phoneNumber, password);
+    res.json({ token });
+  } catch (error) {
+    res.status(401).json({ message: "Credenciales inválidas" });
+  }
+});
+
+routeAuth.get('/', (req, res) => {
+  res.json("Todo bien");
+});
+
+export default routeAuth;
