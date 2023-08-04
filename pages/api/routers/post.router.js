0c9976953fedc2bf50bc1b6aa7b5a9420596ca5f
AntonioBatisa <antoniobatistae@gmail.com>
2023-08-04 18:49:06 -0400

    test, no changes to code

--- pages/api/routers/post.router.js
+++ pages/api/routers/post.router.js
@@ -1,7 +1,35 @@
 import express from 'express'
-import { createPost, getPostDetail } from '../usecases/post.usecases'
+import { createPost, getPostDetail } from '../usecases/post.usecases.js'
+import multer from 'multer'
+import path from 'path'
 
 const routerPost = express.Router()
+
+
+// multer middleware
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, './images/'))
+  },
+  filename: function (req, file, cb) {
+          cb(null, file.fieldname + '-' + Date.now() + file.originalname.match(/\..*$/)[0])
+  }
+});
+const multi_upload = multer({
+  storage,
+  limits: { fileSize: 1 * 1024 * 1024 }, // 1MB
+  fileFilter: (req, file, cb) => {
+      if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+          cb(null, true);
+      } else {
+          cb(null, false);
+          const err = new Error('Only .png, .jpg and .jpeg format allowed!')
+          err.name = 'ExtensionError'
+          return cb(err);
+      }
+  },
+}).array('uploadedImages', 5)
 
 routerPost.post('/', (req, res)=>{
 
