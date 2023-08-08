b5f140c6ce27c71265347484058a8706ca3a64a5
Kim Ramirez <kim.rc.1066@gmail.com>
2023-08-08 00:38:22 -0600

    style: footer css

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
       <div className='justify-content-between px-5  d-flex'>
         <img className={`${styles.logo} ms-5`} src='logo-yali.svg' alt='logo-yali' width={100}></img>
        
-        <ul className={`${styles.list}   me-5 d-flex`}>
+        <ul className={`${styles.list} text-center  me-5 d-flex`}>
           <li><a className={`${styles.a} a`} href='#'>Inicio</a></li>
           <li><a className={`${styles.a} mx-4  a`} href='#'>Adopta</a></li>
           <li><a className={`${styles.a} a`} onClick={()=>push('/signup?role=rescatist')}>Dar en adopción</a></li>
@@ -34,9 +34,9 @@ const Footer = () => {
 
       <hr className={styles.hr}></hr>
 
-        <div className=' d-flex justify-content-around'>
+        <div className=' d-flex justify-content-around '>
 
-        <small className='cols-sm-12 col-6 '>Copyright © Yali 2023. Todos los derechos reservados</small>
+        <small className='cols-sm-12 col-6 ms-3'>Copyright © Yali 2023. Todos los derechos reservados</small>
         <small><a className={`${styles.a} a me-3 col-sm-12 col-lg-3 `} href='#'>Aviso de Privacidad</a></small>
         <small><a className={`${styles.a} a col-sm-12 col-lg-3`} href='#'> Términos y Condiciones</a></small>
 
