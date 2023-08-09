ddadf82dc201ff65252543c0a618ae1b10a151ea
Iyari Maldonado <mffchmono@gmail.com>
2023-08-08 22:14:49 -0600

    feat: added more front end elements

--- pages/login.jsx
+++ pages/login.jsx
@@ -12,24 +12,23 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
 
-      setErrorMessage('Credenciales inválidas. Por favor, verifica tu número de teléfono y contraseña.')
+    setErrorMessage('Credenciales inválidas. Por favor, verifica tu número de teléfono y contraseña.')
+    setErrorMessage('')
+    try {
+      const response = await axios.post('http://localhost:8080/login', {
+        phoneNumber,
+        password,
+      })
       setErrorMessage('')
-      try {
-        const response = await axios.post('http://localhost:8080/login', {
-          phoneNumber,
-          password,
-        })
-        setErrorMessage('')
-        const token = response.data.token
-        localStorage.setItem('token', token)
-        window.location.href = '/dashboard'
-      } catch (error) {
-        console.error('Error al iniciar sesión:', error.response.data.message)
-        setErrorMessage('Credenciales inválidas. Por favor, verifica tu número de teléfono y contraseña.')
-      }
+      const token = response.data.token
+      localStorage.setItem('token', token)
+      window.location.href = '/dashboard'
+    } catch (error) {
+      console.error('Error al iniciar sesión:', error.response.data.message)
+      setErrorMessage('Credenciales inválidas. Por favor, verifica tu número de teléfono y contraseña.')
     }
-  
-    
+  }
+
 
   return (
     <div className="bg-light min-vh-100">
@@ -40,13 +39,17 @@ const Login = () => {
           </a>
         </div>
       </nav>
+      <section className='vh-100 vw-100 px-lg-5 d-flex align-items-center justify-content-center'>
 
-      <div className="bg-light min-vh-100 d-flex align-items-center justify-content-center">
-          <div className="col-md-4">
-            <div className="card p-4" style={{ borderRadius: '8px' }}>
-              <h2 className="mb-4">Inicio de Sesión</h2>
-              {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
-          <form onSubmit={handleSubmit}>
+
+        <form
+          onSubmit={handleSubmit}
+          className='col-10 col-md-6 col-lg-4 col-xl-3 bg-white d-flex flex-column justify-content-between px-4 py-3 rounded-4 shadow'
+        >
+          
+          <h3 className='text-center fs-4 mb-3 w-700'>Iniciar Sesión</h3>
+          {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
+          <div className='form-group mb-3'>
             <div className="mb-3">
               <label htmlFor="phoneNumber" className="form-label">
                 Número de teléfono:
@@ -58,31 +61,29 @@ const Login = () => {
                 value={phoneNumber}
                 onChange={(e) => setPhoneNumber(e.target.value)}
               />
-            </div>
 
-            <div className="mb-3">
-              <label htmlFor="password" className="form-label">
-                Contraseña:
-              </label>
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Contraseña:
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
             </div>
-
-            <button type="submit" className="btn btn-primary w-100">
+          </div>
+          <button type="submit" className="btn btn-primary w-100">
               Iniciar Sesión
             </button>
             <button type="button" className="btn btn-secondary w-100 mt-3">
               Iniciar sesión con Google
             </button>
-          </form>
-        </div>
-      </div>
-    </div>
+        </form>
+      </section>
 
       <footer className="mt-4 text-center">
         <p>© {new Date().getFullYear()} Tu Sitio Web. Todos los derechos reservados.</p>
