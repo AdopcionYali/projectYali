08d30a3ddeb377e30668cc6d2a811ce34bbab672
JesusMendoza815 <jesuscamposmendoza815@gmail.com>
2023-08-10 01:22:55 -0600

    feat: mobile version

--- pages/dashboard/rescatist.jsx
+++ pages/dashboard/rescatist.jsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { useAuth } from '@/contexts/auth.context'
 import Navbar from '@/components/Navbar'
 import styles from '@/styles/DashRescatist.module.scss'
 import dogFingerprint from '@/public/icon-dog-fingerprint.svg'
 import dogs from '@/public/dogs-dashboard-rescatist.png'
 import iconUser from '@/public/icon-user.svg'
-import { useAuth } from '@/contexts/auth.context'
 
 const inputs = [
   {
@@ -21,6 +22,12 @@ const inputs = [
     placeholder: 'Introduce tu fecha de nacimiento',
   },
   {
+    label: 'Correo electrónico',
+    name: 'email',
+    type: 'email',
+    placeholder: 'Introduce tu código postal',
+  },
+  {
     label: 'Código postal',
     name: 'zipcode',
     type: 'text',
@@ -30,28 +37,22 @@ const inputs = [
     label: 'Municipio',
     name: 'town',
     type: 'select',
-    placeholder: 'Introduce tu municipio',
-    active: true
+    placeholder: 'De acurdo a código postal',
+    active: true,
   },
   {
     label: 'Estado',
     name: 'phone',
     type: 'text',
-    placeholder: 'Introduce tu estado',
-    active: true
-  },
-  {
-    label: 'Correo electrónico',
-    name: 'email',
-    type: 'email',
-    placeholder: 'Introduce tu código postal',
+    placeholder: 'De acurdo a código postal',
+    active: true,
   },
 ]
 
 export default function Rescatist() {
-
+  const [previewImg, setPreviewImg] = useState('')
   const { user } = useAuth()
-  
+
   const {
     register,
     handleSubmit,
@@ -60,36 +61,44 @@ export default function Rescatist() {
   } = useForm()
 
   const onSubmit = () => {
-    console.log('ENVIADO');
-    console.log(getValues());
+    console.log('ENVIADO')
+    console.log(getValues())
   }
 
   return (
     <>
       <Navbar />
       <main
-        className={`vw-100 mt-4 d-flex justify-content-evenly ${styles.main}`}
+        className={`vw-100 mt-lg-4 d-flex flex-column flex-lg-row justify-content-evenly ${styles.main}`}
       >
-        <aside className={`${styles.lateral_menu} bg-color-secondary col-lg-2 text-white rounded-4 px-5 py-5`}>
-          <h4 className='w-700 mb-5'>
+        <aside
+          className={`${styles.lateral_menu} bg-color-secondary col-12 px-3 py-3 col-lg-2 text-white rounded-4 px-lg-5 py-lg-5`}
+        >
+          <h4 className='w-700 mb-lg-5'>
             <i className='bi bi-pencil-square me-2'></i>
             Mi perfil
           </h4>
-          <p className='fs-5'>
+          <p className='fs-5 m-0 m-lg-auto text-center text-lg-start'>
             <i className='bi bi-people me-2'></i>
             Menú
           </p>
-          <div className='d-flex flex-column ms-2'>
-            <button className='ms-1 my-2 ps-2 py-1 rounded-2'>Publicar</button>
-            <button className='ms-1 my-2 ps-2 py-1 rounded-2'>Solicitudes</button>
-            <button className='ms-1 my-2 ps-2 py-1 rounded-2'>Mascotas</button>
+          <div className='d-flex flex-lg-column mt-2 pt-2 mt-lg-auto pt-lg-auto ms-lg-2'>
+            <button className='ms-0 text-center text-lg-start ms-lg-1 my-lg-2 ps-lg-2 py-1 rounded-2'>
+              Publicar
+            </button>
+            <button className='ms-0 text-center text-lg-start ms-lg-1 my-lg-2 ps-lg-2 py-1 rounded-2'>
+              Solicitudes
+            </button>
+            <button className='ms-0 text-center text-lg-start ms-lg-1 my-lg-2 ps-lg-2 py-1 rounded-2'>
+              Mascotas
+            </button>
           </div>
         </aside>
 
-        <section className='bg-white-1 col-lg-8 rounded-4 px-5 py-3 shadow'>
+        <section className='bg-white-1 col-lg-8 rounded-4 mx-3 mx-lg-0 px-4 px-lg-5 py-3 shadow'>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <h1 className='w-500'>
-              Hola <span className='w-700 color-primary'>Rescatista!</span>
+            <h1 className='w-400'>
+              Hola <span className='w-700 color-primary'>Rescatista! </span>
             </h1>
             <p className='w-600 text-black-50'>
               *Tu cuenta ha sido creada. Para continuar con el proceso de
@@ -98,13 +107,10 @@ export default function Rescatist() {
             </p>
 
             <div className='row d-flex'>
-              <article className='col-5 d-flex flex-column'>
+              <article className='col-12 col-lg-5 d-flex flex-column'>
                 {inputs.map(({ name, type, label, placeholder, active }, i) => (
-                  <div
-                    className='form-group mt-3'
-                    key={`input-${name}-${i}`}
-                  >
-                    <label className='fw-normal' htmlFor={name}>
+                  <div className='form-group mt-3' key={`input-${name}-${i}`}>
+                    <label className='fw-normal mb-2 mb-lg-auto' htmlFor={name}>
                       {label}
                     </label>
                     <input
@@ -113,32 +119,45 @@ export default function Rescatist() {
                       id={name}
                       placeholder={placeholder}
                       disabled={active}
-                      { ...register(name, { required: true }) }
+                      {...register(name, { required: true })}
                     />
                   </div>
                 ))}
               </article>
 
-              <article className='col-4 d-flex flex-column justify-content-between'>
-                <div className='form-group'>
-                  <p className='text-justify'>
-                    Sube una foto tuya sosteniendo tu identificación oficial
-                    vigente para corroborar tu identidad.
-                  </p>
-                  <input type='file' className='form-control my-2' { ...register('photoIdUrl', { required:true } ) } />
-                  <div className={`${styles.logo_user_container} rounded-4 p-3`}>
-                    <img
-                      src={iconUser.src}
-                      alt='user-image'
-                    />
-                  </div>
+              <article className='col-12 col-lg-4 d-flex flex-column justify-content-between'>
+                <p className='mt-4 mt-lg-auto'>
+                  Sube una foto tuya sosteniendo tu identificación oficial
+                  vigente para corroborar tu identidad.
+                </p>
+                <div className={`${styles.input_file_container} rounded-2 mb-2 d-flex align-items-center justify-content-center`}>
+                  <label htmlFor="photoIdUrl">Seleccionar archivo</label>
+                  <i className="bi bi-plus-circle ms-2" />
+                  <input
+                    id='photoIdUrl'
+                    type='file'
+                    className='form-control'
+                    {...register('photoIdUrl', { required: true })}
+                    onChange={(e) => {
+                      setPreviewImg(URL.createObjectURL(e.target.files[0]))
+                    }}
+                  />
                 </div>
-                <div className='form-check'>
+                <div
+                  className={`${styles.logo_user_container} p-2 rounded-4`}
+                >
+                  <img
+                    src={previewImg || iconUser.src}
+                    alt='user-image'
+                    className='rounded-4'
+                  />
+                </div>
+                <div className='form-check my-4 my-lg-auto'>
                   <input
                     id='privacy'
                     type='checkbox'
                     className='form-check-input'
-                    { ...register('privacy', { required: true }) } 
+                    {...register('privacy', { required: true })}
                   />
                   <label htmlFor='privacy' className='form-check-label'>
                     Acepto el aviso de privacidad y los términos y condiciones
@@ -148,10 +167,10 @@ export default function Rescatist() {
                 <div className='form-group'>
                   <button
                     type='submit'
-                    className='bg-color-primary text-white w-100 rounded-2 py-2 px-2 btn_validation fw-bold border-0'
-                    onClick={ (e) => console.log(isValid) }
+                    className='bg-color-primary text-white w-100 rounded-2 py-1 px-2 btn_validation fw-bold border-0'
+                    onClick={(e) => console.log(isValid)}
                   >
-                    Solicitar validación
+                    Solicitar verificación
                     <Image
                       src={dogFingerprint.src}
                       width={30}
