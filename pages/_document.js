a3bfacf4932812624ef75409d82d3c436bd6193c
JesusMendoza815 <jesuscamposmendoza815@gmail.com>
2023-08-16 01:28:39 -0600

    fix: ASCII to UTF-8 for decoded token

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,18 @@ export default function Document() {
   return (
     <Html lang='es'>
       <Head>
-      <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
+        <meta charSet='UTF-8' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='anonymous'
+        />
         <link
           href='https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900&family=Raleway:wght@100;200;300;400;500;600;700;800&display=swap'
           rel='stylesheet'
         />
-      </ Head>
+      </Head>
       <body className='bg-white-2'>
         <Main />
         <NextScript />
