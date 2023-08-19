3224582df604bc76d4bbcc9da38381a8663923f6
AntonioBatisa <antoniobatistae@gmail.com>
2023-08-19 02:43:11 -0400

    feat: added multer functionality to front end

--- services/post.services.js
+++ services/post.services.js
@@ -1,18 +1,28 @@
-const savePostObject = async (postData) => {
-  try {
-    let response = await fetch("http://localhost:8080/", {
-      method: "POST",
-      mode: "no-cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ postData }),
-    });
-    let data = await response.json();
-    if (!data.success) throw new Error("Error");
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
 
+const savePostObject = async (postData, files) => {
+  let postDataJSON = JSON.stringify(postData)
+  try {
+    let response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/post`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: postDataJSON
+  })
+  let data = await response.json()
+  console.log(data)
+  if (!data.success) throw new Error('Error')
+
+  let id = data.postId
+  let patchResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/post/${id}/uploads`, {
+  method: 'POST',
+  body: files
+ })
+ let imgResp = await patchResponse.json()
+ console.log(files, imgResp, 'console log de respuesta en segunda peticion')
+ return data, imgData
+} catch (error) {
+  return error
+}}
+
+export default savePostObject
